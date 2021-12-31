9ea3ebde0a9702cb768740567146d57a0f53adc9
MsNiyaJ <shaniyasmalcolm@gmail.com>
2021-12-31 04:28:11 -0500

    removed unnecessary divs

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -9,20 +9,18 @@ const Projects = () => {
         <h1 className="text-3xl md:text-4xl text-center">
           My Work & Portfolio
         </h1>
-        <div className="mt-8">
-          <div className=" flex flex-wrap justify-center">
-            {ProjectItems.map((proj) => {
-              return (
-                <Project
-                  key={proj.name}
-                  name={proj.name}
-                  url={proj.url}
-                  src={proj.src}
-                  image={proj.image}
-                />
-              );
-            })}
-          </div>
+        <div className="pt-10 flex flex-wrap justify-center">
+          {ProjectItems.map((proj) => {
+            return (
+              <Project
+                key={proj.name}
+                name={proj.name}
+                url={proj.url}
+                src={proj.src}
+                image={proj.image}
+              />
+            );
+          })}
         </div>
       </div>
     </div>
