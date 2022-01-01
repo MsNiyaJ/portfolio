f69b4f4a7767a2f17d9ead8ab35540c6f51d85c8
MsNiyaJ <shaniyasmalcolm@gmail.com>
2022-01-01 15:04:26 -0500

    footer added

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="bg-pink-600 text-white py-10 text-center">
+      <div className="container mx-auto">
+        <p>&copy; Shaniya Malcolm 2021. Design by Zoyothemes</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
