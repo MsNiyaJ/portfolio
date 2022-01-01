5708663c3432d1a97007a3832714338dd36f44ae
MsNiyaJ <shaniyasmalcolm@gmail.com>
2022-01-01 15:10:27 -0500

    removed class from download icon

--- src/icons/DownloadIcon.js
+++ src/icons/DownloadIcon.js
@@ -13,7 +13,6 @@ const DownloadIcon = () => {
         strokeWidth="2"
         strokeLinecap="round"
         strokeLinejoin="round"
-        class="feather feather-download fea icon-sm"
       >
         <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
         <polyline points="7 10 12 15 17 10"></polyline>
