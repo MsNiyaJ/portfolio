c84995ccc65272ddd6cb8697635230bf7a568538
MsNiyaJ <shaniyasmalcolm@gmail.com>
2022-01-03 20:24:59 -0500

    cleaned up tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,10 @@ module.exports = {
       'm-sm': '320px',
       'm-md': '375px',
       'm-lg': '425px',
-      sm: '640px',
-      // => @media (min-width: 640px) { ... }
+      sm: '640px', // => @media (min-width: 640px) { ... }
       md: '768px',
       lg: '1024px',
-      // => @media (min-width: 1024px) { ... }
-
       'x-lg': '1280px',
-      // => @media (min-width: 1280px) { ... }
     },
     extend: {
       backgroundImage: {
