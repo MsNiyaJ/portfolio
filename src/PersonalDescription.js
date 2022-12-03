3a09cfe67874450b832a9dc01734874904a67912
MsNiyaJ <shaniyasmalcolm@gmail.com>
2022-12-03 15:27:42 -0500

    updated description

--- src/PersonalDescription.js
+++ src/PersonalDescription.js
@@ -3,13 +3,16 @@ import React from 'react';
 const PersonalDescription = () => {
   return (
     <div className="mt-5 sm:mt-0">
-      <h1 className="md:text-lg font-semibold">
-        I Am a <span className="text-pink-500">Frontend Developer</span>
+      <h1 className="md:text-lg font-semibold text-pink-500">
+        Software Developer
       </h1>
       <p className="md:text-lg mt-2 text-gray-600">
-        Motivated Frontend Developer with experience building and maintaining
-        responsive websites. Proficient in HTML, CSS, JavaScript, and React.
-        Passionate about building beautiful, responsive webpages.
+        I am motivated developer with over a year of experience building and
+        maintaining beautiful, responsive websites. Proficient in React.
+      </p>
+      <p className="md:text-lg pt-2 text-gray-600">
+        <b>Fun fact: </b>I built this website from scratch in four days using
+        React.
       </p>
     </div>
   );
